0292d06598a523f195ce487eac80c30776fc0877
Pruthviraj Jadhav <pruthvirajjadhav1@gmail.com>
2022-09-11 20:28:38 +0530

    Level-3 Hashing

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -23,8 +24,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // Encryption
-const secret = "Thisismysecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+const secret = process.env.SECRET;
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,7 +43,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password), // It creates an Hash and compares to the login hash
   });
 
   newUser.save(function (err) {
@@ -57,7 +57,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
